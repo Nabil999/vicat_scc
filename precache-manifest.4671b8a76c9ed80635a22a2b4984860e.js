1b11bb2754050b78a2980b34425435c6fda33fc5
nabil999 <nabil.farouk@gmail.com>
2020-10-24 03:59:16 +0200

    Updates

---
+++ precache-manifest.4671b8a76c9ed80635a22a2b4984860e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0332fd17f5355488f809213b2a21edb4",
+    "url": "/vicat_scc/index.html"
+  },
+  {
+    "revision": "3282f4e124078032f202",
+    "url": "/vicat_scc/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "61cff51acacfa7c9b036",
+    "url": "/vicat_scc/static/js/2.c9185c28.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/vicat_scc/static/js/2.c9185c28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3282f4e124078032f202",
+    "url": "/vicat_scc/static/js/main.59d0ba7a.chunk.js"
+  },
+  {
+    "revision": "9eab4ddd9453d5777788",
+    "url": "/vicat_scc/static/js/runtime-main.f367bc96.js"
+  },
+  {
+    "revision": "3e16c13d1fa95ea13dceba0c99cbfba0",
+    "url": "/vicat_scc/static/media/vicat_logo.3e16c13d.svg"
+  }
+]);
